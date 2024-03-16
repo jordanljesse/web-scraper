e609f01bfe86d2629af6b1e451006f12ef3e2585
jordan <jordan@JORDANJESSE2685>
2024-03-16 12:04:39 -0400

    Add project files.

---
+++ web-scraper/Program.cs
@@ -0,0 +1,40 @@
+﻿using AngleSharp;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_scraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // use the default configuration for AngleSharp
+            var config = Configuration.Default.WithDefaultLoader();
+
+            // set the context for evaluating web pages with the given configuration
+            var context = BrowsingContext.New(config);
+
+            // web address to be parsed
+            var address = "https://news.ycombinator.com";
+
+            // create an asynchronous request for the specified document to load
+            var document = await context.OpenAsync(address);
+
+            // select the content we want from the document
+            var cellSelector = "span.titleline a";
+            var cells = document.QuerySelectorAll(cellSelector);
+            var titles = cells.Select(m => m.TextContent);
+            var results = document.QuerySelectorAll(cellSelector)
+                .Select(el => el.TextContent);
+
+            // display the results
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
